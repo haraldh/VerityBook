612289f5afa2179c8f50d8a926378d4e789018a0
Harald Hoyer <harald@redhat.com>
2018-09-13 11:30:59 +0200

    mkimage.sh: fix LockDown.efi and Shell.efi copy

--- mkimage.sh
+++ mkimage.sh
@@ -158,8 +158,8 @@ mount ${DEV_PART}1 "$MY_TMPDIR"/boot
 
 mkdir -p "$MY_TMPDIR"/boot/EFI/Boot
 cp "$SOURCE"/bootx64.efi "$MY_TMPDIR"/boot/EFI/Boot/bootx64.efi
-[[ -e ${CURDIR}/Lockdown.efi ]] && cp ${CURDIR}/Lockdown.efi "$MY_TMPDIR"/boot/
-[[ -e ${CURDIR}/Shell.efi ]] && cp ${CURDIR}/Lockdown.efi "$MY_TMPDIR"/boot/
+[[ -e ${CURDIR}/LockDown.efi ]] && cp ${CURDIR}/LockDown.efi "$MY_TMPDIR"/boot/
+[[ -e ${CURDIR}/Shell.efi ]] && cp ${CURDIR}/Shell.efi "$MY_TMPDIR"/boot/
 umount "$MY_TMPDIR"/boot
 
 # ------------------------------------------------------------------------------
